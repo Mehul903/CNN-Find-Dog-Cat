0f72773d99686c287016cef35c8cd1895c9adb91
Mehul Patel <MPatel@legendary.com>
2017-12-01 21:33:38 -0500

    First commit of script.

---
+++ scripts/cnn-cat-dog.py
@@ -0,0 +1,10 @@
+## Import packages for building a CNN:
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+
+## Initialize CNN:
+classifier = Sequential()
