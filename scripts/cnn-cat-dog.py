3f7395c01aab75c8b9358dc3267df1206e8d7e1b
Mehul Patel <MPatel@legendary.com>
2017-12-16 15:03:08 -0500

    Complete CNN added.

--- scripts/cnn-cat-dog.py
+++ scripts/cnn-cat-dog.py
@@ -12,4 +12,79 @@ classifier = Sequential()
 
 ## Flow of CNN:
 ## Convolution --> Max Pooling --> Flattening --> Full Connection
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3)), activation = 'relu')
+
+## Step-1: Add convolutional layer
+## Here (32,3,3) indicates that I'll ues 32 feature detectors and thus will have 
+## 32 feature maps (Can have more if you have a GPU)!. 3x3 indicates #rows x #cols 
+## in a feature detector. In other words, 3x3 is the shape of feature detector.
+
+## (64,64,3) indicates the size of the image (in pixels) that I'll consider is 64 x 64.
+## I can certainly choose 128x128 or 256x256 but I have a CPU, not GPU!
+
+## Considered stride of 1 to go over actual features (i.e. actual image) using 
+## a feature detector.
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+## Step-2: Max-pooling
+## Will consider stride of 2 while max-pooling step. This reduces the size of a 
+## feature map, i.e. we get a new feature map. Actually, the size of a new feature map 
+## (or pooled feature map) is (size of original feature map/2) + 1 (because size of the 
+## original feature map was 5 x 5. If the dimensiona were to be even, then size of the 
+## pooled feature map will be (size of original feature  map)/2.
+
+## One of the reasons for max-pooling is to reduce the size of feature map because it, 
+## in the step of full-connection, decreases the size of neural network by decreasing 
+## the number of nodes in a layer thus making it computationally more efficient.
+## By reducing the size of original feature map, we don't loose performance because 
+## max-pooling captures important information from original feature map.
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+## Step-3: Flattening
+## Take a pooled feature map and put it in a single vector (making it 1-D).
+classifier.add(Flatten())
+
+
+## Step-4: Full connection
+classifier.add(Dense(units = 64, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+## Compile network:
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+## Image preprocessing:
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+                '../dataset/training_set',
+                target_size=(64, 64),
+                batch_size=32,
+                class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+                '../dataset/test_set',
+                target_size=(64, 64),
+                batch_size=32,
+                class_mode='binary')
+
+classifier.fit_generator(
+            training_set,
+            steps_per_epoch=8000,
+            epochs=10,
+            validation_data=test_set,
+            validation_steps=2000)
+
+## Why do we need max-pooled layer in a 1-D array? Why not just put all the pixel values
+## of the image in a 1-D array?
+## Answer: If we only put the pixel values of an image in a 1-D array then each node of
+## neural network contains information about independent pixels, and does not preserve 
+## spatial structure structure of an actual image. However, if we perform previous 
+## steps then we are keeping spatial structure of an image and also reducing the size 
+## of final neural network.
